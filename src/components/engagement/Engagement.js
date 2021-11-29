287450275d14c11aecb395b4457d358b9b669747
Christopher McKenzie <camckenzie94@gmail.com>
2021-11-28 22:42:36 -0500

    testing

--- src/components/engagement/Engagement.js
+++ src/components/engagement/Engagement.js
@@ -1,8 +1,5 @@
-import Button from '@restart/ui/esm/Button';
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
-import { Console, Hook, Unhook } from 'console-feed'
-
 
 function Engagement() {
 
@@ -41,7 +38,7 @@ function Engagement() {
 
     }
     const Record = details => {
-        console.log(details);
+        // console.log(details);
         
         //Need an if else statement here but haven't done yet
         setDetails({
@@ -51,7 +48,30 @@ function Engagement() {
             complexity: details.complexity,
             fte: details.fte,
         })
-        //console.log(details);
+
+
+        //working on this - steven
+        // let i = 2;
+
+        // let template = `
+        //     <h1>Your details:
+        //         <span id="yourdesc"> </span>
+        //         <span id="yourcost"> </span>
+        //         <span id="yourrisk"> </span>
+        //         <span id="yourcomplexity"> </span>
+        //         <span id="yourfte"> </span>
+        //     </h1>
+        // `;
+
+
+
+        console.log(details);
+        document.getElementById("yourdesc").innerHTML += details.desc;
+        document.getElementById("yourcost").innerHTML += details.cost;
+        document.getElementById("yourrisk").innerHTML += details.risk;
+        document.getElementById("yourcomplexity").innerHTML += details.complexity;
+        document.getElementById("yourfte").innerHTML += details.fte;
+
         // setError(console.log(details));
     }
     
@@ -68,42 +88,54 @@ function Engagement() {
 
     
     return (
-    <form onSubmit={submitHandler}>
-        <div className="App">
-            <div className="form-inner">
-                <h2>Engagement Creation</h2>
-                {(error!="") ? ( <div className="error">{error}</div> ) : ""}
-                <div className="form-group">
-                    <label htmlFor="desc">Description: </label>
-                    
-                    <input type="text" name="desc" id="desc" onChange={e => setDetails({...details, desc: e.target.value})} value={details.desc} />
+        <div>
+            <form onSubmit={submitHandler}>
+                <div className="App">
+                    <div className="form-inner">
+                        <h2>Engagement Creation</h2>
+                        {(error!="") ? ( <div className="error">{error}</div> ) : ""}
+                        <div className="form-group">
+                            <label htmlFor="desc">Description: </label>
+                            
+                            <input type="text" name="desc" id="desc" onChange={e => setDetails({...details, desc: e.target.value})} value={details.desc} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="desc">Computed Cost: </label>
+                            
+                            <input type="text" pattern="[0-9]*" name="cost" id="cost" onChange={e => setDetails({...details, cost: e.target.value})} value={details.cost}  />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="desc">Risk: </label>
+                            
+                            <input type="text" pattern="[0-9]*" name="risk" id="risk" onChange={e => setDetails({...details, risk: e.target.value})} value={details.risk}  />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="desc">Complexity: </label>
+                            
+                            <input type="text" pattern="[0-9]*" name="complexity" id="complexity" onChange={e => setDetails({...details, complexity: e.target.value})} value={details.complexity}  />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="desc">Total FTE: </label>
+                            
+                            <input type="text" pattern="[0-9]*" name="fte" id="fte" onChange={e => setDetails({...details, fte: e.target.value})} value={details.fte}  />
+                        </div>
+                        <input type="submit" value="SUBMIT" />
+                    </div>
+                    <div>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="desc">Computed Cost: </label>
-                    
-                    <input type="text" pattern="[0-9]*" name="cost" id="cost" onChange={e => setDetails({...details, cost: e.target.value})} value={details.cost}  />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="desc">Risk: </label>
-                    
-                    <input type="text" pattern="[0-9]*" name="risk" id="risk" onChange={e => setDetails({...details, risk: e.target.value})} value={details.risk}  />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="desc">Complexity: </label>
-                    
-                    <input type="text" pattern="[0-9]*" name="complexity" id="complexity" onChange={e => setDetails({...details, complexity: e.target.value})} value={details.complexity}  />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="desc">Total FTE: </label>
-                    
-                    <input type="text" pattern="[0-9]*" name="fte" id="fte" onChange={e => setDetails({...details, fte: e.target.value})} value={details.fte}  />
-                </div>
-                <input type="submit" value="SUBMIT" />
-
-                
+            </form>
+            <div id="yourdetails">
+                <h1>Your details:
+                    <span id="yourdesc"> </span>
+                    <span id="yourcost"> </span>
+                    <span id="yourrisk"> </span>
+                    <span id="yourcomplexity"> </span>
+                    <span id="yourfte"> </span>
+                </h1>
             </div>
+
         </div>
-    </form>
 
     );
 }
